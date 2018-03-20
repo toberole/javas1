013b58c05e23c2c04e43fc43ec083515209a0740
zhouwei <zhouwei11@58ganji.com>
2018-03-20 10:49:42 +0800

    fixed untracked files

--- src/com/zw/test/Sort.java
+++ src/com/zw/test/Sort.java
@@ -262,9 +262,5 @@ public class Sort {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
     }
 }
