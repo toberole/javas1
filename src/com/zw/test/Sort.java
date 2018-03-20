7cf15db89c386b581983a7357d139516a5c0491c
zhouwei <zhouwei11@58ganji.com>
2018-03-20 10:45:14 +0800

    gitignore

--- src/com/zw/test/Sort.java
+++ src/com/zw/test/Sort.java
@@ -262,5 +262,9 @@ public class Sort {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
