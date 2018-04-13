e20f9c622edf7a19d8561e10869d3d3d30fc6dd0
zhouwei <zhouwei11@58ganji.com>
2018-04-13 14:33:17 +0800

    Iterator 实现list一边遍历一边删除

--- src/com/zw/test/Test01.java
+++ src/com/zw/test/Test01.java
@@ -1,6 +1,12 @@
 package com.zw.test;
 
 
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Test01 {
     static {
         System.out.println("=======");
@@ -46,5 +52,23 @@ public class Test01 {
         System.out.println(new Object().hashCode());
 
 
+        List list = new ArrayList<String>();
+
+        list.add("1");
+        list.add("2");
+        list.add("123");
+        list.add("4");
+
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String s = (String) iterator.next();
+            if (s.equals("123")) {
+                iterator.remove();
+            }
+        }
+
+        for (int j = 0; j < list.size(); j++) {
+            System.out.println(list.get(j));
+        }
     }
 }
